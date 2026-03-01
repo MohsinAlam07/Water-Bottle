dbfe7b3405429dde80bd872ec6ea54bfb261f752
Md Mohsin Alam <mohsinalam1141@gmail.com>
2026-03-01 22:08:25 +0600

    Buy btn

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { use } from 'react';
 import Bottle from '../Bottle/Bottle';
+import './Bottles.css'
 
 const Bottles = ({bottlesPromise}) => {
+
+
     const bottles=use(bottlesPromise);
     // console.log(bottles)
+   const [cart,setCart]=useState([])
+    const handleAddToCart=(bottle)=>{
+        console.log(bottle);
+    }
+
+    
+
     return (
         <div>
             <h3>Bottles : {bottles.length}</h3>
-            {
-                bottles.map(bottle=><Bottle bottle={bottle} key={bottle.id}></Bottle>)
+          <div className='bottles-container'>
+              {
+                bottles.map(bottle=><Bottle
+                     bottle={bottle} 
+                     handleAddToCart={handleAddToCart}
+                     key={bottle.id}></Bottle>)
             }
+          </div>
             
         </div>
     );
